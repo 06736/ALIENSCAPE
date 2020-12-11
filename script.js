5c6b56093d3c9291dd040b74a79a4b30d7c69192
bmynard <06736@farnborough.ac.uk>
2020-12-11 21:52:43 +0000

    Has working main menu button, js needs a bit of touching (order-wise)

--- script.js
+++ script.js
@@ -1,31 +1,40 @@
 
+
+
+function main_menu() {
+    $(document).ready(function () {
+        $(".h1").fadeOut("2000", function () {
+            $(".h1").empty();
+        });
+        $(".p1").fadeOut("2000", function () {
+            $(".p1").empty();
+
+        });
+        $(".btn").fadeOut("2000");
+    });
+}
 function courtyard_initial(){
-    console.log("Slowly, you open your eyes. You are in the middle of a courtyard, enclosed between four ancient dusty walls." +
+
+    $(".p1").append("Slowly, you open your eyes. You are in the middle of a courtyard, enclosed between four ancient dusty walls." +
         "You look around. Everything around you is decayed and rotten, as if it had only died very recently. The entire courtyard stinks of " +
         "midlew. Upon closer inspection, you notice that the cobblestone underfoot has been cracked, like there had been a recent impact." +
-        "\nType into the box 'Look Around' to see what happens");
-    let look_around_validation = true;
+        "Type into the box 'Look Around' to see what happens").fadeIn("normal");
+}
+function play_the_game(){
 
+    //fades out all the text on the screen
 
-        let look_around_courtyard = prompt("");
-        look_around_courtyard.toLowerCase();//case insensitive
-        if(look_around_courtyard === "look around"){
-            console.log("");
-            look_around_validation = false;
-        }else{
-            console.log("Try Retyping...")
-        }
+    main_menu();
+
+    setTimeout("courtyard_initial()", 2000)
+    courtyard_initial();
 
 
 }
 
 
 
-function play_the_game() {
 
-
-}
-play_the_game();
 
 
 
