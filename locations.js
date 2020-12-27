ecba0aadb28a8df0559f637b7221505593ff6cbd
bmynard <06736@farnborough.ac.uk>
2020-12-27 15:40:48 +0000

    works for more stages however possibility that not more than one input can be taken per room, will fix next patch

--- locations.js
+++ locations.js
@@ -1,5 +1,12 @@
 let locations = [
-    {
+    {//index 0
+        name: "courtyard initial",
+        description: "Slowly, you open your eyes. You are in the middle of a courtyard, enclosed between four ancient dusty walls." +
+            "You look around. Everything around you is decayed and rotten, as if it had only died very recently. The entire courtyard stinks of " +
+            "midlew. Upon closer inspection, you notice that the cobblestone underfoot has been cracked, like there had been a recent impact."
+
+    },
+    {//index 1
         name: "Courtyard",
         description: "Everything around you seems to have died recently. Every weed, patch of grass \n" +
             "and vein of ivy is now rotten and decaying, as if it had been alive recently.\n" +
@@ -9,25 +16,32 @@ let locations = [
             "East there is a door with a moon crest atop it,\n" +
             "South there is a door with a sun crest atop it,\n" +
             "And west there is a door with a black hole atop it.\n" +
-            "and directly beneath your feet, there is a trapdoor, which you cannot open..."
+            "and directly beneath your feet, there is a trapdoor, which you cannot open...",
+        requirement: "none"
 
     },
-    {
+    {//index 2
         name: "The Star Room",
         description:"You open the doors. Inside, you are met with a small circular room. The ceiling\n" +
             "was peppered with stars, each one individulistic and yet equal. There is a small \n" +
             "brown door at the far end of the room, with a feeble looking lock on it." +
             "To the right, there is a door which leads to the Moon room.\n" +
-            "To the left side, there is a small metal desk with an upturned chair  \n"
+            "To the left side, there is a small metal desk with an upturned chair  \n",
+        requirement: "none",
+        on_look_around: "You move to the far side of the room, and turn around. Instantly, you notice three out-of-place objects. A crystalline Spyglass. A Dark Matter Capsule." +
+            " \nA small, thumbnail sized metallic fragment of sorts. You move to the metal desk and rummage through the contents of the drawer. Your hands brush away some paper" +
+            ", and there sits a rusty handgun, fully loaded."
+
     },
-    {
+    { // index 3
         name: "The Crafting Room",
         description: "You shoot out the lock with the handgun, the recoil of which is horrid and jolts +\n" +
             " \"your arm. You continue down a very short passage until you reach a small room. +\n" +
             " \"Inside, there is a large 3D printer, with trays to put the materials inside of.  +\n" +
             " \"On top of which, there is a manual with all the items you can craft with it.\n",
+        requirement: "rusty handgun",
     },
-    {
+    {// index 4
         name: "The Moon Room",
         description: "You open the door to the moon room. It is a large, square room, with eerily \n" +
             "modern walls for such an old building. You step inside, and a harsh white light \n" +
@@ -39,15 +53,17 @@ let locations = [
             "scratch it off.\n" +
             "SP*CEM*N FR*M  *I**N\n" +
             "The rock here was a deep shade of black. You put your head closer.\n" +
-            "The rock is covered in pinpricks of pulsating, purple light"
+            "The rock is covered in pinpricks of pulsating, purple light",
+        requirement: "none",
     },
-    {
+    { // index 5
         name: "The Sun Room",
         description: "You enter the Sun Room. Inside, there is a miniscule nuclear fusion reactor, \n" +
             "small enough to fit in the palm of a human hand. It has a small red button on it,  \n" +
             "which states 'Power On'. You decide not to click it due to how rusty it looks.",
+        requirement: "none",
     },
-    {
+    { // index 6
         name: "The Black Hole Room",
         description: "You turn the key, and gently push open the door. You are met again with a harsh\n" +
             "white light. There is a single pedestal in the centre of the room. However, there is \n" +
@@ -58,9 +74,10 @@ let locations = [
             "The collector is also much heavier now. Another prompt shows up on the screen:\n" +
             "'PRESS HERE TO CONCENTRATE'. You press again, and instantaneously, \n" +
             "a small black ball is in the middle of the capsule. To the far side of the room, \n" +
-            "there is another door with a sign next to it, stating: 'DO NOT ENTER'"
+            "there is another door with a sign next to it, stating: 'DO NOT ENTER'",
+        requirement: "black key",
     },
-    {
+    { // index 7
         name: "Do Not Enter Room",
         description: "You enter the room. You are met with silence, then... a distant humming\n" +
             "You walk down the corridor, handgun out, then reach a clearing. In the center\n" +
@@ -70,12 +87,14 @@ let locations = [
             "charged at you. It leaps, and bites onto your arm. With your other hand, you grab\n" +
             "your gun from the holster and shoot it, once to the head. Instantly, the blue light \n" +
             "dies down. You glimpse at the box. It begins to hum again. You run",
+        requirement: "in the black hole room"
     },
-    {
+    { // index 8
         name: "The Mirror Room",
         description: "You descend down the old ladder, which can hardly hold your weight, as it \n" +
             "creaks so loudly. Once fully descended, a light switches on, and all you can see\n" +
-            "is a big mirror. You take a glimpse of yourself, and remember everything.\n"
+            "is a big mirror. You take a glimpse of yourself, and remember everything.\n",
+        requirement: "a set of armour",
     },
     {
             }
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-
-
-
+//sets the current location
+let current_location = "courtyard initial";
+let inventory = [];
 function main_menu() {
     $(document).ready(function () {
         $(".h1").fadeOut("2000", function () {
@@ -20,8 +20,7 @@ function new_input(){
     input.setAttribute("id", "input");
     input.addEventListener("keyup",function(event){
         if(event.which == 13){
-            let x = $("input").val();
-            console.log(x);
+            on_input(input);
         }
     });
     document.body.appendChild(input);
@@ -32,15 +31,60 @@ function new_input(){
 
 
 function courtyard_initial(){
-    $(".p1").append("Slowly, you open your eyes. You are in the middle of a courtyard, enclosed between four ancient dusty walls." +
-        "You look around. Everything around you is decayed and rotten, as if it had only died very recently. The entire courtyard stinks of " +
-        "midlew. Upon closer inspection, you notice that the cobblestone underfoot has been cracked, like there had been a recent impact." +
-        "Type into the box 'Look Around' to see what happens").fadeIn("normal");
+    $(".p1").append(locations[0]["description"]).fadeIn("normal");
     new_input();
 
 
 
+}
+function on_input(input){
+    //will run until the user types "quit"
+    let quit = false;
+    while(quit === false) {
+        let user_input = $("input").val().toLowerCase();
+        $(input).val("");
+        switch (current_location){
+            case "courtyard initial":
+                if(user_input === "look around"){
+                    $(".p1").empty().append(locations[1]["description"]);//adds the description for the courtyard
 
+                    current_location = locations[1]["name"];
+                }
+                break;
+            case "Courtyard":
+                if (user_input === "go north"){ // the star room
+                    current_location = locations[2]["name"];
+                    $(".p1").empty().append(locations[2]["description"])
+                }else if(user_input === "go east"){ // the moon room
+                    current_location = locations[4]["name"];
+                }else if (user_input === "go south"){ // the sun room
+                    current_location = locations[5]["name"];
+                }else if(user_input === "go west"){ // the black hole room
+
+                }
+                break;
+            case "The Star Room":
+                if (user_input === "look around"){
+                    $(".p1").empty().append(locations[2]["on_look_around"])
+                }
+                break;
+            case "The Crafting Room":
+                break;
+            case "The Moon Room":
+                break;
+            case "The Sun Room":
+                break;
+            case "The Black Hole Room":
+                break;
+            case "The Do Not Enter Room":
+                break;
+            case "The Mirror Room":
+                break;
+        }
+
+
+        quit = true;
+    }
 }
 function play_the_game(){
 
